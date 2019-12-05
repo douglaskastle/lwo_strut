d5fa7dea640a00474290e924357825d3e4d0b1c0
Douglas Kastle <douglas.kastle@gmail.com>
2019-12-05 14:46:20 +0000

    Added failing example for pytest top catch correctly

--- lwo_parse.py
+++ lwo_parse.py
@@ -18,10 +18,23 @@ def main():
     with open(outfile, 'rb') as handle:
         b = pickle.load(handle)
     
-    #x.pprint()
+    x.pprint()
     #b.pprint()
     print(x == b)
     
+    #x.layers[0].name = "Layer 2"
+    outfile = infile + ".error0.pickle"
+    #os.unlink(outfile)
+    if not os.path.isfile(outfile):
+        with open(outfile, 'wb') as f:
+            pickle.dump(x, f, pickle.HIGHEST_PROTOCOL)
+    
+    with open(outfile, 'rb') as handle:
+        b = pickle.load(handle)
+    
+    #x.pprint()
+    #b.pprint()
+    print(x == b)
 
 if __name__ == '__main__':
     main()
--- tests/basic/test_load_lwo.py
+++ tests/basic/test_load_lwo.py
@@ -2,7 +2,7 @@ import os
 import pickle
 from lwo_strut.lwoObject import lwoObject
 
-def test_load_lwo_box0():
+def test_load_lwo_box0_pass():
     infile = "tests/basic/src/LWO2/box/box0.lwo"
     x = lwoObject(infile)
     x.read()
@@ -13,3 +13,15 @@ def test_load_lwo_box0():
         b = pickle.load(handle)
     
     assert x == b
+    
+def test_load_lwo_box0_fail():
+    infile = "tests/basic/src/LWO2/box/box0.lwo"
+    x = lwoObject(infile)
+    x.read()
+    
+    outfile = infile + ".error0.pickle"
+    
+    with open(outfile, 'rb') as handle:
+        b = pickle.load(handle)
+    
+    assert not x == b
