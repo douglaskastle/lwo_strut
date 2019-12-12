04d0527594e267e74949827921d63886f17d4b8e
Douglas Kastle <douglas.kastle@gmail.com>
2019-12-12 17:06:09 +0000

    Update test for logging

--- tests/lwo_interceptor/test_load_lwo_interceptor.py
+++ tests/lwo_interceptor/test_load_lwo_interceptor.py
@@ -2,7 +2,7 @@ from lwo_strut.lwoObject import lwoObject
 from scripts.lwo_helper import LwoFile
 
 
-def test_load_lwo_box0_pass():
+def test_load_lwo_interceptor():
     infile = "tests/lwo_interceptor/src/LWO2/Federation - Interceptor/objects/interceptor_hull.lwo"
     f = LwoFile(infile)
     f.check_file()
@@ -14,5 +14,7 @@ def test_load_lwo_box0_pass():
 
     f.setup_pickle(x)
     b = f.load_pickle()
+    
+    x == b
 
     assert x == b
